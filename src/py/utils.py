dfb8a8ee97a39e092f9f03a5544215c41db5aee7
lucieDLE <dolelucie0@gmail.com>
2024-09-10 10:04:47 -0400

    ENH: add mixup function and focal loss

--- src/py/utils.py
+++ src/py/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
-import itk
+import torch 
+import torch.nn as nn
 
 def GetImage(img_np, ctype = 'float'):
 	img_np_shape = np.shape(img_np)
@@ -44,3 +45,48 @@ def GetImage(img_np, ctype = 'float'):
 	out_img_np.setfield(img_np.reshape(out_img_np.shape), out_img_np.dtype)
 
 	return out_img
+
+import pdb
+def mixup_img_seg(x, seg, y, alpha=0.5):
+	batch_size = x.shape[0]
+
+	yhot = nn.functional.one_hot(y, num_classes=2)
+	
+	lam = np.random.beta(alpha, alpha, (batch_size,)).astype(dtype=np.float32)
+	lam = torch.from_numpy(lam).to(x.device)
+
+	index = torch.randperm(batch_size)
+	lam = lam.view(batch_size, *[1]*x[0].dim())
+
+	x_perm = torch.stack([x[idx,...] for idx in index])
+	seg_perm = torch.stack([seg[idx,...] for idx in index])
+	
+	mixed_x = lam * x + (1 - lam) * x_perm
+	mixed_seg = lam * seg + (1 - lam) * seg_perm
+
+	lam = lam.view(batch_size, 1)
+	mixed_y = lam * yhot + (1 - lam) * yhot[index, ...]
+
+
+	return  {"img":mixed_x, "seg":mixed_seg, "class":mixed_y}
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, reduction='mean', weights =None):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+        self.weights = weights
+
+    def forward(self, inputs, targets):
+        BCE_loss = nn.functional.binary_cross_entropy_with_logits(inputs, targets, reduction='none', weight=self.weights)
+        pt = torch.exp(-BCE_loss)
+        F_loss = self.alpha * (1-pt)**self.gamma * BCE_loss
+
+        if self.reduction == 'mean':
+            return F_loss.mean()
+        elif self.reduction == 'sum':
+            return F_loss.sum()
+        else:
+            return F_loss
