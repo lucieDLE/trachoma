3a6e4df1192b6a886d32014796f4ec382ef90a25
lucieDLE <dolelucie0@gmail.com>
2024-12-16 17:31:12 -0500

    WIP: faster rcnn

--- src/py/object_detection_train.py
+++ src/py/object_detection_train.py
@@ -1,5 +1,5 @@
 import os
-# os.environ['CUDA_VISIBLE_DEVICES']="0"
+os.environ['CUDA_VISIBLE_DEVICES']="0"
 import argparse
 
 import math
@@ -20,6 +20,24 @@ from lightning.pytorch.loggers import NeptuneLogger, TensorBoardLogger
 
 from sklearn.utils import class_weight
 
+def remove_labels(df, args):
+
+    if args.drop_labels is not None:
+        df = df[ ~ df[args.label_column].isin(args.drop_labels)]
+
+    if args.concat_labels is not None:
+        replacement_val = df.loc[ df['label'] == args.concat_labels[0]]['class'].unique()
+        df.loc[ df['label'].isin(args.concat_labels), "class" ] = replacement_val[0]
+
+    unique_classes = sorted(df[args.class_column].unique())
+    class_mapping = {value: idx+1 for idx, value in enumerate(unique_classes)}
+    print(class_mapping)
+
+    df[args.class_column] = df[args.class_column].map(class_mapping)
+    df.loc[ df[args.label_column] == 'Reject', args.class_column]  = 0
+    print(f"{df[[args.label_column, args.class_column]].drop_duplicates()}")
+    return df.reset_index()
+
 def main(args):
 
 
@@ -27,11 +45,17 @@ def main(args):
     df_val = pd.read_csv(args.csv_valid)
     df_test = pd.read_csv(args.csv_test)
 
-    df_test = df_test.loc[~df_test['label'].isin(['Healthy', 'Reject'])].reset_index()
-    df_val = df_val.loc[~df_val['label'].isin(['Healthy', 'Reject'])].reset_index()
-    df_train = df_train.loc[~df_train['label'].isin(['Healthy', 'Reject'])].reset_index()
+    df_train = remove_labels(df_train, args)
+    df_val = remove_labels(df_val, args)
+    df_test = remove_labels(df_test, args)
 
-    num_classes = len(df_train['label'].unique()) +1
+    # df_test = df_test.loc[~df_test['label'].isin(['Healthy', 'Reject'])].reset_index()
+    # df_val = df_val.loc[~df_val['label'].isin(['Healthy', 'Reject'])].reset_index()
+    # df_train = df_train.loc[~df_train['label'].isin(['Healthy', 'Reject'])].reset_index()
+
+    num_classes = len(df_train[args.class_column].unique())
+
+    print(df_train[args.class_column].value_counts())
 
     ttdata = TTDataModuleBX(df_train, df_val, df_test, batch_size=args.batch_size, num_workers=args.num_workers, img_column=args.img_column, mount_point=args.mount_point)
 
@@ -40,7 +64,8 @@ def main(args):
         dirpath=args.out,
         filename='{epoch}-{val_loss:.2f}',
         save_top_k=2,
-        monitor='val_loss'
+        monitor='val_loss',
+        save_last=True,
     )
 
     image_logger = FasterRCNNImageLoggerNeptune(log_steps = args.log_every_n_steps)
@@ -84,6 +109,9 @@ if __name__ == '__main__':
     input_group.add_argument('--csv_test', required=True, type=str, help='Test CSV')
     input_group.add_argument('--img_column', type=str, default="img_path", help='Name of image column in csv')
     input_group.add_argument('--class_column', type=str, default="seg_path", help='Name of segmentation column in csv')
+    input_group.add_argument('--label_column', type=str, default="label", help='Name of label column in csv')
+    input_group.add_argument('--drop_labels', type=str, default=None, nargs='+', help='drop labels in dataframe')
+    input_group.add_argument('--concat_labels', type=str, default=None, nargs='+', help='concat labels in dataframe')
 
     hparams_group = parser.add_argument_group('Hyperparameters')
     hparams_group.add_argument('--lr', '--learning-rate', default=1e-4, type=float, help='Learning rate')
@@ -91,6 +119,8 @@ if __name__ == '__main__':
     hparams_group.add_argument('--patience', help='Max number of patience steps for EarlyStopping', type=int, default=30)
     hparams_group.add_argument('--steps', help='Max number of steps per epoch', type=int, default=-1)    
     hparams_group.add_argument('--batch_size', help='Batch size', type=int, default=256)
+
+
     
     logger_group = parser.add_argument_group('Logger')
     logger_group.add_argument('--log_every_n_steps', help='Log every n steps', type=int, default=1)
