d43c6e3de01db1078f610e0fd591c7f7de22471b
lucieDLE <dolelucie0@gmail.com>
2025-09-17 13:40:12 -0400

    enh: improve heights matching and borders processing

--- src/py/process_heights.py
+++ src/py/process_heights.py
@@ -35,6 +35,18 @@ def adjust_array(bottom, top):
   return keep_bottom, new_top
 
 
+def resample_top_curve(points, target_len):
+    
+    diffs = np.diff(points, axis=0)
+    seg_lengths = np.hypot(diffs[:,0], diffs[:,1])
+    dist = np.concatenate([[0], np.cumsum(seg_lengths)])
+    new_dist = np.linspace(0, dist[-1], target_len)
+    
+    x_new = np.interp(new_dist, dist, points[:,0])
+    y_new = np.interp(new_dist, dist, points[:,1])
+    
+    return np.column_stack((x_new, y_new))
+
 def compute_distances_and_matches(contour_1, contour_2, smooth_factor=14):
 
     distances=[]
@@ -75,8 +87,8 @@ def main(args):
   df = pd.read_csv(args.csv)
 
   mount_point=args.mount_point
-  i=1466
-  for idx, row in df[1466:].iterrows():
+  i=0
+  for idx, row in df[i:].iterrows():
     print(f"index {i}/{len(df)}")
     i+=1
     # pdb.set_trace()
@@ -107,15 +119,33 @@ def main(args):
       cnt_bottom, cnt_top = adjust_array(cnt_bottom, cnt_top)
       cnt_top, cnt_bottom = adjust_array(cnt_top, cnt_bottom)
 
+      ## adjust borders -
+      max_left_y = np.argmax(cnt_top[:300,1])
+      max_right_y = np.argmax(cnt_top[-300:,1])
+      pos_left = cnt_top.shape[0] - 300
+
+      cnt_top = cnt_top[max_left_y:pos_left+max_right_y]
+
+      ## downsample bottom
+      # cnt_bottom_down = cnt_bottom[np.linspace(0, len(cnt_bottom)-1, cnt_top.shape[0], dtype=int)]
+
+      ## upsample top ( seems to be slightly better than downsampling bottom)
+      cnt_top_resampled = resample_top_curve(cnt_top, target_len=cnt_bottom.shape[0])
+      
+      diff = cnt_top_resampled - cnt_bottom
+      squared_diff = np.square(diff)
+      sum_squared_diff = np.sum(squared_diff,axis=1)
+      distance = np.sqrt(sum_squared_diff)
+
       # plt.subplot(122)
       # plt.plot(cnt_top[:,0], cnt_top[:,1], 'xg')
       # plt.plot(cnt_bottom[:,0], cnt_bottom[:,1], 'xr')
       # plt.title('after adjustment')
 
-      distances, x_values, matches = compute_distances_and_matches(cnt_bottom, cnt_top)
+      # distances, x_values, matches = compute_distances_and_matches(cnt_bottom, cnt_top)
 
-      dic_out = {'x_values': x_values, 'heights':distances}
-      outfile = seg_path.replace('seg', 'segmentation_heights').replace('.nrrd', '.npy')
+      dic_out = {'x_top': cnt_top_resampled[:,0], 'x_bottom':cnt_bottom[:,0], 'heights':distance}
+      outfile = seg_path.replace('seg', 'segmentation_heights_v2').replace('.nrrd', '.npy')
       np.save(outfile, dic_out)
       ## saving the raw output so in script can choose how many point we sampled.
   # else:
